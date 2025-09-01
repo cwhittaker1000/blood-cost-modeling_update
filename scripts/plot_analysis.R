e374155f0a3bb294efbf1f24223333eaa99f3f61
harm0n <harmonprograms@protonmail.com>
2025-09-01 17:58:25 -0400

    updated, but will need to change more

--- scripts/plot_analysis.R
+++ scripts/plot_analysis.R
@@ -1,3 +1,4 @@
+source("functions.R")
 library(tidyverse)
 library(furrr)
 library(scales)
@@ -15,6 +16,7 @@ opt_seq_depth_summary <- read_tsv("../results/summarized_results.tsv")
 
 # Extract data for a single mew closest to our estimated mew, and remove any
 # parameter combinations that did not converge (hit our ceiling)
+
 select_mew_read_threshold_data <- opt_seq_depth_summary %>%
   filter(mew == 1e-6, read_threshold == 100) %>%
   mutate(
@@ -22,6 +24,7 @@ select_mew_read_threshold_data <- opt_seq_depth_summary %>%
     hit_ceiling = optimal_depth >= 1e12
   ) %>%
   filter(!hit_ceiling)
+
 depth_plot <- ggplot(
   select_mew_read_threshold_data,
   aes(
@@ -33,29 +36,42 @@ depth_plot <- ggplot(
 ) +
   geom_point(size = 3) +
   geom_line(linewidth = 1, linetype = "solid") +
-  scale_x_log10(breaks = c(0.0001, 0.001, 0.005, 0.01, 0.05, 0.1)) +
+  scale_x_log10(
+    breaks = c(0.0001, 0.001, 0.005, 0.01, 0.05, 0.1),
+    labels = function(x) sprintf("%g", x),
+    #    labels = label_number(accuracy = 0.0001, trim = TRUE),
+    limits = c(0.0001, 0.1)
+  ) +
   scale_y_log10(
     labels = trans_format("log10", math_format(10^.x)),
     limits = c(NA, 1e12)
   ) +
   scale_colour_brewer(
-    palette = "Accent",
-    name = "Number of individuals \nin weekly sequencing\nbatch"
+    palette = "Dark2",
+    name = "# of individuals"
   ) +
   labs(
     title = "",
-    y = "Sequencing depth per week to detect HIV-like pathogen",
+    y = "Sequencing depth (Weekly, $)",
     x = "Cumulative incidence (%)"
   ) +
   theme_bw(base_size = 15) +
   theme(
-    plot.title = element_text(hjust = .5, face = "bold"),
     plot.subtitle = element_text(hjust = .5),
-    legend.position = "right"
+    axis.title = element_text(size = 14),
+    axis.text = element_text(size = 13),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 11),
+    legend.box.background = element_rect(colour = "black"),
+    # Remove panel border, but keep axis
+    panel.border = element_blank(),
+    axis.line = element_line(colour = "black"),
+    #    legend.position = "right"
+    legend.position = c(0.15, 0.7)
   )
 depth_plot
 
-ggsave("../output/depth_plot.png", depth_plot, width = 8, height = 6)
+ggsave("../output/depth_plot.png", depth_plot, width = 8, height = 4)
 
 
 ## Cost analysis for a specific depth
@@ -67,7 +83,7 @@ sample_c <- 15
 select_mew_read_threshold_data_with_cost <- select_mew_read_threshold_data %>%
   filter(!hit_ceiling) %>% # Exclude points that didn't converge
   mutate(
-    total_cost_per_year = calculate_total_cost_per_year(
+    total_cost_per_year = calc_total_cost_per_year(
       seq_depth = optimal_depth,
       cost_of_seq = seq_c,
       cost_of_proc = proc_c,
@@ -75,6 +91,7 @@ select_mew_read_threshold_data_with_cost <- select_mew_read_threshold_data %>%
       num_samples = batch_size
     )
   )
+
 cost_plot <- ggplot(
   select_mew_read_threshold_data_with_cost,
   aes(
@@ -86,29 +103,42 @@ cost_plot <- ggplot(
 ) +
   geom_line(linewidth = 1) +
   geom_point(size = 3) +
-  scale_x_log10(breaks = c(0.001, 0.005, 0.01, 0.05, 0.1)) +
+  scale_x_log10(
+    breaks = c(0.0001, 0.001, 0.005, 0.01, 0.05, 0.1),
+    labels = function(x) sprintf("%g", x),
+    #    labels = label_number(accuracy = 0.0001, trim = TRUE),
+    limits = c(0.0001, 0.1)
+  ) +
   scale_y_log10(
     breaks = c(10000000, 100000000),
     labels = trans_format("log10", math_format(10^.x))
   ) +
   scale_colour_brewer(
-    palette = "Accent",
-    name = "Number of individuals \nin weekly sequencing\nbatch"
+    palette = "Dark2",
+    name = "# of individuals"
   ) +
   labs(
     title = "",
-    y = "Total cost per year to detect HIV-like pathogen ($)",
+    y = "Sequencing depth (Weekly, $)",
     x = "Cumulative incidence (%)"
   ) +
   theme_bw(base_size = 15) +
   theme(
-    plot.title = element_text(hjust = .5, face = "bold"),
     plot.subtitle = element_text(hjust = .5),
-    legend.position = "right"
+    axis.title = element_text(size = 14),
+    axis.text = element_text(size = 13),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 11),
+    legend.box.background = element_rect(colour = "black"),
+    # Remove panel border, but keep axis
+    panel.border = element_blank(),
+    axis.line = element_line(colour = "black"),
+    #    legend.position = "right"
+    legend.position = c(0.15, 0.7)
   )
 cost_plot
 
-ggsave("../output/cost_plot.png", cost_plot, width = 8, height = 6)
+ggsave("../output/cost_plot.png", cost_plot, width = 8, height = 4)
 
 # Only filter by read threshold so that we can show a plot
 # comparing different mews
@@ -133,25 +163,34 @@ facet_depth_plot <- ggplot(
   geom_point(
     size = 3,
   ) +
-  scale_x_log10(breaks = c(0.001, 0.005, 0.01, 0.05, 0.1)) +
+  scale_x_log10(
+    breaks = c(0.0001, 0.001, 0.005, 0.01, 0.05, 0.1),
+    labels = function(x) sprintf("%g", x),
+    #    labels = label_number(accuracy = 0.0001, trim = TRUE),
+    limits = c(0.0001, 0.1)
+  ) +
   scale_y_log10(
     labels = trans_format("log10", math_format(10^.x)),
     limits = c(NA, 1e12)
   ) +
   scale_colour_brewer(
-    palette = "Accent",
-    name = "Number of individuals \nin weekly sequencing\nbatch"
+    palette = "Dark2",
+    name = "# of individuals"
   ) +
   labs(
     title = "",
-    y = "Sequencing depth per week to detect HIV-like pathogen",
+    y = "Sequencing depth (Weekly, $)",
     x = "Cumulative incidence (%)"
   ) +
   theme_bw(base_size = 15) +
   facet_grid(. ~ mew) +
   theme(
-    plot.title = element_text(hjust = .5, face = "bold"),
     plot.subtitle = element_text(hjust = .5),
+    axis.title = element_text(size = 14),
+    axis.text = element_text(size = 13),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 11),
+    legend.box.background = element_rect(colour = "black"),
     legend.position = "right"
   )
 facet_depth_plot
@@ -159,8 +198,8 @@ facet_depth_plot
 ggsave(
   "../output/facet_mew_depth_plot.png",
   facet_depth_plot,
-  width = 25,
-  height = 8
+  width = 30,
+  height = 6
 )
 
 
@@ -172,14 +211,13 @@ select_mew_read_threshold_data_with_varied_costs <- select_read_threshold_data %
   filter(!hit_ceiling) %>% # Exclude points that didn't converge
   crossing(sample_cost = sample_costs) %>% # Create all combinations
   mutate(
-    total_cost_per_year = find_total_cost(
+    total_cost_per_year = calc_total_cost_per_year(
       seq_depth = optimal_depth,
       cost_of_seq = seq_c,
       cost_of_proc = proc_c,
       cost_of_sample = sample_cost,
       num_samples = batch_size
-    ) *
-      52
+    )
   )
 
 # Compare costs for different mews with different sample costs
@@ -194,23 +232,32 @@ facet_cost_plot <- ggplot(
 ) +
   geom_line(linewidth = 1) +
   geom_point(size = 3) +
-  scale_x_log10(breaks = c(0.001, 0.005, 0.01, 0.05, 0.1)) +
+  scale_x_log10(
+    breaks = c(0.0001, 0.001, 0.005, 0.01, 0.05, 0.1),
+    labels = function(x) sprintf("%g", x),
+    #    labels = label_number(accuracy = 0.0001, trim = TRUE),
+    limits = c(0.0001, 0.1)
+  ) +
   scale_y_log10(labels = trans_format("log10", math_format(10^.x))) +
   scale_colour_brewer(
-    palette = "Accent",
-    name = "Number of individuals \nin weekly sequencing\nbatch"
+    palette = "Dark2",
+    name = "# of individuals"
   ) +
   #  facet_wrap(~ paste0("Sample cost: $", sample_cost), scales = "free_y") +
   facet_grid(sample_cost ~ mew) +
   labs(
-    title = "Annual cost with varying sample collection costs",
-    y = "Total cost per year to detect HIV-like pathogen ($)",
+    title = "",
+    y = "Sequencing depth (Weekly, $)",
     x = "Cumulative incidence (%)"
   ) +
   theme_bw(base_size = 12) +
   theme(
-    plot.title = element_text(hjust = .5, face = "bold"),
     plot.subtitle = element_text(hjust = .5),
+    axis.title = element_text(size = 14),
+    axis.text = element_text(size = 13),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 11),
+    legend.box.background = element_rect(colour = "black"),
     legend.position = "right"
   )
 facet_cost_plot
@@ -218,6 +265,6 @@ facet_cost_plot
 ggsave(
   "../output/facet_cost_plot.png",
   facet_cost_plot,
-  width = 25,
+  width = 30,
   height = 15
 )
